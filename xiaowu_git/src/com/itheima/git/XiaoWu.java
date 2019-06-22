76ce93f911a33cfeccc8368df9a9b481a7f08760
zhangsan <zhangsan@qq.com>
2019-06-22 20:52:36 +0800

    添加本地

---
+++ xiaowu_git/src/com/itheima/git/XiaoWu.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class XiaoWu {
+    public static void main(String[] args) {
+        System.out.println("今天请假啊啊啊啊");
+        System.out.println("今天请假啊啊啊啊");
+        System.out.println("今天请假啊啊啊啊");
+        System.out.println("今天请假啊啊啊啊");
+    }
+}
