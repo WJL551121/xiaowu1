0e61b1f62535bf1e2fd98c0ea79b2f71709e640a
zhangsan <zhangsan@qq.com>
2019-06-22 21:19:07 +0800

    更新本地

--- xiaowu_git/src/com/itheima/git/XiaoWu.java
+++ xiaowu_git/src/com/itheima/git/XiaoWu.java
@@ -7,4 +7,9 @@ public class XiaoWu {
         System.out.println("今天请假啊啊啊啊");
         System.out.println("今天请假啊啊啊啊");
     }
+    public void test(){
+        int i = 10;
+        i++;
+        System.out.println(i);
+    }
 }
